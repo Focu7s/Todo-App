3f4531210a0221f8a2e6c9929a52630a1c5a7c0b
Focu7s <141554054+Focu7s@users.noreply.github.com>
2025-06-26 22:01:09 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+let todo = []; // empty array
+
+let req = prompt("please enter your request"); // prompt is stored in variable req
+
+while (true) {
+    if (req == "quit") {
+        console.log("quitting app");
+        break;
+    }
+
+    if (req == "list") {
+        console.log("-------------");
+        for (let i = 0; i < todo.length; i++) {
+            console.log(i, todo[i]);
+        }
+        console.log("-------------");
+    } else if (req == "add") {
+        let task = prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    } else if (req == "delete") {
+        let idx = prompt("please enter the index of the task you want to delete"); // ✅ fixed message
+        todo.splice(idx, 1);
+        console.log("task deleted");
+    } else {
+        console.log("wrong request");
+    }
+
+    req = prompt("please enter your request");
+}
